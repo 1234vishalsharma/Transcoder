b8705d6a73f65430c6e23777913bf66cabb7916e
Vishal Sharma <vishalsharma15122003@gmail.com>
2024-04-07 09:58:47 +0530

    Upload issue

--- frontend/src/components/card.jsx
+++ frontend/src/components/card.jsx
@@ -1,6 +1,36 @@
 import React from "react";
+import { useState } from "react";
+import axios from 'axios';
 
 function Card() {
+
+  const [Video , setVideo] = useState();
+
+  const UploadHandeler = async() => {
+
+    if(Video)  console.log(Video.name);
+
+    
+    try{
+      
+      const formData = new FormData();
+      formData.append('video', Video);
+  
+      const response = await axios.post('http://localhost:3000/backend/Upload', formData, {
+      headers: {
+        'Content-Type': 'multipart/form-data'
+      },
+    });
+
+    if(!response.ok){
+      console.log("cannot uploaded file");
+    }
+    
+  }
+    catch{
+      console.log("Error in Uploading the file")
+    }
+  }
   return (
     <div className="flex flex-col gap-4 items-center h-96 p-8 rounded-lg bg-slate-800">
       <h1 className="text-center text-2xl text-blue-600">Put your video here</h1>
@@ -14,10 +44,12 @@ function Card() {
             className="hidden text-white gap-3"
             type="file"
             id="input_video"
+            onChange = { (event) => { setVideo(event.target.files[0]) }}
           />
         </label>
       </div>
-      <button className="text-white bg-slate-500 p-2 rounded-md border-2 border-white hover:bg-slate-700 mt-4">
+      <button className="text-white bg-slate-500 p-2 rounded-md border-2 border-white hover:bg-slate-700 mt-4"
+      onClick={UploadHandeler}>
         Upload
       </button>
     </div>
