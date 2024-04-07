ed434b42283c33361396e9c0eeaf88c3b24aa064
shivam121q <shivampal7006@gmail.com>
2024-04-07 09:57:20 +0530

    Temp change

---
+++ frontend/src/components/FilePreview.jsx
@@ -0,0 +1,21 @@
+// eslint-disable-next-line no-unused-vars
+import React from 'react'
+
+function FilePreview() {
+  return (
+    <div className=''>
+       <div className='w-3/4 h-3/4 bg-slate-900 border flex justify-center items-center '>
+         <div className=''>
+
+         </div>
+        <div className=''>
+             
+        </div>
+
+       </div>
+
+    </div>
+  )
+}
+
+export default FilePreview
