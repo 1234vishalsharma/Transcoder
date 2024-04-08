333e4fa7e13f44b24de8674a5a9655ccf912c1fc
shivam121q <shivampal7006@gmail.com>
2024-04-08 11:29:13 +0530

    Temp change

--- backend/controller/Upload.js
+++ backend/controller/Upload.js
@@ -1,24 +1,38 @@
+const { spawn } = require("child_process");
+const path = require("path");
+const fs = require("fs");
+const ffmpeg = require("fluent-ffmpeg");
+// const ffmpeg = require("ffmpeg")
 
+exports.Upload = async (req, res) => {
+  try {
+    // try block which shows the functioning of the uploading the file in backend.
+    // console.log(req.files)
+    const videoName = req.files.video;
+    console.log(videoName);
+    const Fid = Date.now();
 
+    const direc = __dirname + "/videos/" + `${Fid}` + ".mp4";
 
-exports.Upload = async (req,res) => {
-    try{
-        // try block which shows the functioning of the uploading the file in backend.
-        console.log(req.files)
-        const videoName = req.files.video;
+    videoName.mv(direc, (err) => {
+      console.log(err);
+    });
+    
+    
 
-        console.log(videoName)
+    return res.status(200).json({
+      status: true,
+      message: "Video uploaded",
+    });
+  } catch (error) {
+    return res.status(500).json({
+      status: false,
+      message: "Video not uploaded",
+      error: error.message,
+    });
+  }
+};
 
-        return res.status(200).json({
-            success:true,
-            message:"File uploaded successfully",
-        })
-        
-    }
-    catch( error){
-        return res.status(500).json({
-            status: false,
-            message: "Video not uploaded",
-        })
-    }
+function convertVideo(inputFile, outputFile) {
+  // Create the ffmpeg command
 }
